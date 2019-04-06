be6be6a6c2ecddfef136673c32a88f3d2df649bd
Unknown <voxel.tracer@gmail.com>
2019-04-06 08:32:43 -0700

    64B lanes

--- spheres/scene.h
+++ spheres/scene.h
@@ -17,7 +17,7 @@
 
 using namespace std;
 
-const unsigned int lane_size_float = 128 / sizeof(float);
+const unsigned int lane_size_float = 64 / sizeof(float);
 const unsigned int lane_size_spheres = lane_size_float / 3;
 const unsigned int lane_padding_float = lane_size_float - lane_size_spheres * 3;
 
